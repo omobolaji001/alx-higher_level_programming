1426a1be40306f002d18385f988d89dfeb8908fe
omobolaji001 <omobolajiolayiwola11@gmail.com>
2023-07-11 11:18:01 -0700

    5-base_geometry.py

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-""" 
-a function that returns True if the object is an instance 
-of a class that inherited (directly or indirectly) from 
+"""
+a function that returns True if the object is an instance
+of a class that inherited (directly or indirectly) from
 the specified class ; otherwise False.
 """
 
