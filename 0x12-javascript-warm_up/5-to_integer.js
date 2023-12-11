aa445c2709d3a1d9b8f08827d83c3f69ee492e7a
omobolaji001 <omobolajiolayiwola11@gmail.com>
2023-12-11 23:23:42 +0100

    5-to_integer.js

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
 
 const value = Math.floor(Number(process.argv[2]));
-console.log(isNaN(value) ? 'Not a number' : 'My number: ${value}');
+console.log(isNaN(value) ? 'Not a number' : `My number: ${value}`);
