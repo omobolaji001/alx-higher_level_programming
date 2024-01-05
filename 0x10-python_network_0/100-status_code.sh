6359657d194ad159794b84fbff6e4b76bd64a694
Oyebode Habeeb Omobolaji <63916225+omobolaji001@users.noreply.github.com>
2024-01-05 22:37:23 +0100

    Delete 0x10-python_network_0 directory

--- 0x10-python_network_0/100-status_code.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# displays only the status code of http response
-curl -s -o /dev/null -w "%{http_code}" "$1"
--- 0x10-python_network_0/2-delete.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# sends a DELETE request to the given URL, and displays the body of the response
-curl -sX DELETE "$1"
