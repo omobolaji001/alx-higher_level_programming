4a3517e35729532d3243e95cf18c75671d932eef
omobolaji001 <omobolajiolayiwola11@gmail.com>
2024-02-07 23:22:20 +0100

    web jquery

---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,5 @@
+$(document).ready(function () {
+  $.get('https://swapi-api.alx-tools.com/api/films/?format=json', function (data) {
+    $('UL#list_movies').append(...data.results.map(movie => `<li>${movie.title}</li>`));
+  });
+});
